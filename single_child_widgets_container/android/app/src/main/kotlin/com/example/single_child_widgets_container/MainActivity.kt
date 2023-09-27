6628ca0f1cbc041449509f7ec365b72a3987496b
Babul Mirdha <babumirdha@gmail.com>
2023-09-28 05:42:35 +0600

    My First App

---
+++ single_child_widgets_container/android/app/src/main/kotlin/com/example/single_child_widgets_container/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.single_child_widgets_container
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ single_child_widgets_container/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyButton(),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("My Title")),
+      body: Center(
+          child: Container(
+            padding: const
+            EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
+            decoration: const BoxDecoration(
+              border: Border(
+                top: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
+                left: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
+                right: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
+                bottom: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
+              ),
+              color: Colors.grey,
+            ),
+            child: const Text(
+                'OK',textAlign: TextAlign.center, style: TextStyle(color: Colors.black)
+            ),
+          ),
+      ),
+    );
+  }
+}
+
+
+
