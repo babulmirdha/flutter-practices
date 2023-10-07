a0c7dc4f27bb69a84b8a6e3a5121796dbee92da6
Babul Mirdha <babumirdha@gmail.com>
2023-10-07 16:26:44 +0600

    tap example

--- gesture_detector_examples/lib/main.dart
+++ gesture_detector_examples/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(  const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -31,95 +31,106 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  // Handle onTap event here
+                  print('Widget tapped!');
+                },
+                onTapDown: (TapDownDetails details) {
+                  // Handle onTapDown event here
+                  print('Pointer tapped down at ${details.globalPosition}');
+                },
+                onTapUp: (TapUpDetails details) {
+                  // Handle onTapUp event here
+                  print('Pointer tapped up at ${details.globalPosition}');
+                },
+                onDoubleTap: () {
+                  // Handle onDoubleTap event here
+                  print('Widget double tapped!');
+                },
+                onLongPress: () {
+                  // Handle onLongPress event here
+                  print('Widget long pressed!');
+                },
+                onTapCancel: () {
+                  // Handle onTapCancel event here
+                  print('Tap gesture canceled');
+                },
+                child: Container(
+                  height: 200,
+                  color: Colors.blue,
+                  child: const Center(
+                    child: Text('Tap Me!'),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onVerticalDragStart: (DragStartDetails details) {
+                  // Handle onVerticalDragStart event here
+                  print('Vertical drag started at ${details.globalPosition}');
+                },
+                onVerticalDragUpdate: (DragUpdateDetails details) {
+                  // Handle onVerticalDragUpdate event here
+                  print('Vertical drag updated. Delta: ${details.primaryDelta}');
+                },
+                onVerticalDragEnd: (DragEndDetails details) {
+                  // Handle onVerticalDragEnd event here
+                  if (details.primaryVelocity! > 0) {
+                    print(
+                        'Vertical drag ended with positive velocity (downward motion).');
+                  } else if (details.primaryVelocity! < 0) {
+                    print(
+                        'Vertical drag ended with negative velocity (upward motion).');
+                  } else {
+                    print('Vertical drag ended with zero velocity.');
+                  }
+                },
+                child: Container(
+                  height: 200,
+                  color: Colors.amberAccent,
+                  child: const Center(
+                    child: Text('Drag Me Vertically!'),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onHorizontalDragStart: (DragStartDetails details) {
+                  // Handle onHorizontalDragStart event here
+                  print('Horizontal drag started at ${details.globalPosition}');
+                },
+                onHorizontalDragUpdate: (DragUpdateDetails details) {
+                  // Handle onHorizontalDragUpdate event here
+                  print(
+                      'Horizontal drag updated. Delta: ${details.primaryDelta}');
+                },
+                onHorizontalDragEnd: (DragEndDetails details) {
+                  // Handle onHorizontalDragEnd event here
+                  if (details.primaryVelocity !> 0) {
+                    print(
+                        'Horizontal drag ended with positive velocity (rightward motion).');
+                  } else if (details.primaryVelocity! < 0) {
+                    print(
+                        'Horizontal drag ended with negative velocity (leftward motion).');
+                  } else {
+                    print('Horizontal drag ended with zero velocity.');
+                  }
+                },
+                child: Container(
+                  height: 200,
+                  color: Colors.blue,
+                  child: const Center(
+                    child: Text('Drag Me Horizontally!'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
